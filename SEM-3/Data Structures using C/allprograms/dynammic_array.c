c6184290e635a1098fd7f9f4c214fd5ff206493f
Abhish <abhishashokshetty@gmail.com>
2026-01-08 11:16:39 +0530

    added a new segment

--- SEM-3/Data Structures using C/allprograms/dynammic_array.c
+++ SEM-3/Data Structures using C/allprograms/dynammic_array.c
@@ -8,13 +8,14 @@ int main(){
     printf("Enter the size of the array\n");
     scanf("%d",&size);
     array=(int *)malloc(size*sizeof(int));
+    int *q=array;
     printf("enter array elements\n");
     for(int i=0;i<size;i++){
         scanf("%d",array+i);
     }
     printf("Successful!\n");
     for(int i=0;i<size;i++){
-        printf("%d ",*(array+i));
+        printf("%d ",*(q+i));
     }
 
     printf("\nprinting done\n");
