a35d8816574a131e5f98033bcde15a1251257789
Abhish <abhishashokshetty@gmail.com>
2026-01-22 21:02:46 +0530

    Structure program

--- SEM-3/Data Structures using C/Lab codes/structure.c
+++ SEM-3/Data Structures using C/Lab codes/structure.c
@@ -1,86 +1,64 @@
 #include<stdio.h>
+#include<ctype.h>
 #include<stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include<string.h>
 
-int n;
-int size=0;
-typedef struct student{
-    char name[20];
-    char usn[20];
-    char dept[10];
-    int marks;
-}student;
-student *s=NULL;
+int size;
+struct student{
+  char name[30];
+  char usn[30];
+  char dept[30];
+  int marks;
+};
+struct student* s;
+
 void initialise(){
-    s=(struct student *)malloc(n*sizeof(struct student));
-    if(s==NULL){
-        printf("Dynamic allocation failed\n");
-        exit(0);
+  s=(struct student*)malloc(size*sizeof(struct student));
+  if(s==NULL){
+    printf("Allocation failed\n");
+    return ;
+  }
+}
+void addStudent(){
+  for(int i=0;i<size;i++){
+    printf("Enter Details of student %d\n",i+1);
+    printf("Enter Name\n");
+    scanf("%s",s[i].name);
+    printf("Enter usn\n");
+    scanf("%s",s[i].usn);
+    printf("Enter department\n");
+    scanf("%s",s[i].dept);
+    printf("Enter total marks\n");
+    scanf("%d",&s[i].marks);
+  }
+  printf("Students data stored successfully");
+}
+void display(){
+    printf("student details:\n");
+    for(int i=0;i<size;i++){
+        printf("Student name : %s  USN : %s  Department : %s  Marks : %d \n",s[i].name,s[i].usn,s[i].dept,s[i].marks);
     }
 }
-
-void addstudent(){
-    size++;
-    if(size>n){
-        printf("Student record is full");
-        size-=1;
-        return ;
-    }
-    printf("Enter student USN");
-    scanf("%s",s[size-1].usn);
-    printf("Enter student Name");
-    scanf("%s",s[size-1].name);
-    printf("Enter student department");
-    scanf("%s",s[size-1].dept);
-    printf("Enter student marks");
-    scanf("%d",&s[size-1].marks);
-    printf("Data entered successfully\n");
-}
-void searchstudent(){
-    char key[20];
-    printf("Enter student's USN");
+void search(){
+    char key[30];
+    printf("Enter the student USN\n");
     scanf("%s",key);
     for(int i=0;i<size;i++){
-        if(strcmp(s[i].usn,key)==0){
-            printf("\n--------------------------------------------\n");
-            printf("Student USN:%s\nStudent Name:%s\nDept:%s\nMarks:%d",s[i].usn,s[i].name,s[i].dept,s[i].marks);
-            printf("\n----------------------------------------------\n");
+        if(strcmp(key,s[i].usn)==0){
+            printf("Student details found in the database:\n");
+            printf("Student name : %s  USN : %s  Department : %s  Marks : %d \n",s[i].name,s[i].usn,s[i].dept,s[i].marks);
             return ;
         }
     }
-    printf("Student record not found!\n");
-    return ;
+    printf("Student details not found, check USN\n");
 }
-void display(){
-    if(size==0){
-        printf("Empty!\n");
-        return ;
-    }
-    for(int i=0;i<size;i++){
-        printf("\n---------------------------------------------------------------------------\n");
-         printf("Student USN:%s\nStudent Name:%s\nDept:%s\nMarks:%d\n",s[i].usn,s[i].name,s[i].dept,s[i].marks);
-    }
-}
+
 int main(){
-    int choice;
-    printf("Enter the number of students\n");
-    scanf("%d",&n);
+    printf("Enter number of students to store\n");
+    scanf("%d",&size);
     initialise();
-   while(1){
-    printf("1.Add Student\n2.Search student\n3.Display\n4.Exit\n");
-    printf("Enter your choice\n");
-    scanf("%d",&choice);
-    switch(choice){
-        case 1 :addstudent();
-        break;
-        case 2:searchstudent();
-        break;
-        case 3:display();
-        break;
-        case 4:exit(0);
-        default : printf("Invalid choice\n");
-    }
-   }
-    return 1;
+    addStudent();
+    display();
+    search();
+    return 0;
 }
